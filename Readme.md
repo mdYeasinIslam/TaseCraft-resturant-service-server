98bd169f90af897f575ffe934e06b8a702a56354
mdYeasinIslam <hyeasinislam@gmail.com>
2025-01-15 23:36:54 +0600

    tasteCraft server side build and some api integrate for fatching data

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+
+db_user=TasteCraft_resturant
+db_pass=brZeRhci0rYWJYwV
---
+++ index.js
@@ -0,0 +1,66 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000
+
+
+app.use(cors())
+app.use(express.json())
+// TasteCraft_resturant
+// brZeRhci0rYWJYwV
+
+
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.bfv30pl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //   await client.connect();
+      const database = client.db('TasteCraft_resturant').collection('menuItems')
+      const shopItemCollection = client.db('TasteCraft_resturant').collection('shopItems')
+
+    //menu items collect from db and send to the client side
+      app.get('/menuItem', async (req, res) => {
+          const result = await database.find().toArray()
+          res.send(result)
+      })
+    // get each shop-item from client and insert to the db
+    app.post('/shopCarts', async (req, res) => {
+      const body = req.body;
+      console.log(body)
+      const result = await shopItemCollection.insertOne(body)
+      res.send({success:true,message:'Successfully add to the db'})
+    })
+    //get shopCarts item from db and send to the client
+    app.get('/shopCarts', async (req, res) => {
+      const result = await shopItemCollection.find().toArray()
+      res.send(result)
+    })
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+  catch (error) {
+   console.log(error)
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('resturant server is running') 
+})
+
+app.listen(port,()=>console.log(`running on ${port}`))
