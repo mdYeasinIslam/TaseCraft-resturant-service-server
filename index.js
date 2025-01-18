ab3044653c914433fa9cf417535a51e4dab07d9f
mdYeasinIslam <hyeasinislam@gmail.com>
2025-01-18 17:29:00 +0600

    jwt token , varify midleware create and secure confidential api calls

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
@@ -8,8 +9,23 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-// TasteCraft_resturant
-// brZeRhci0rYWJYwV
+
+const varifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({message:"forbidden access"})
+  }
+  const token = req.headers.authorization?.split(' ')[1]
+  jwt.verify(token,process.env.jwt_secret, (err, decode) => {
+    if (err) {
+      return res.status(401).send({message:"forbidden access"})
+    }
+    else {
+      req.decoded =decode
+    next()
+    }
+    
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.bfv30pl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -30,7 +46,15 @@ async function run() {
       const database = client.db('TasteCraft_resturant').collection('menuItems')
       const shopItemCollection = client.db('TasteCraft_resturant').collection('shopItems')
       const usersCollection = client.db('TasteCraft_resturant').collection('users')
-
+    
+    //---------jwt api------------
+    app.post('/jwt', async (req, res) => {
+      const userInfo = req.body
+      const token = jwt.sign(userInfo,process.env.jwt_secret, { expiresIn: '1h' })
+      res.send(token)
+    })
+   
+    
     //menu items collect from db and send to the client side
       app.get('/menuItem', async (req, res) => {
           const result = await database.find().toArray()
@@ -72,10 +96,35 @@ async function run() {
       const result = await usersCollection.insertOne(body)
       res.send(result)
     })
-    app.get('/users', async (req, res) => {
+
+    //varify jwt token when get users data from db and send to the client
+    //can use a midlewire
+    app.get('/users', varifyToken, async (req, res) => {
+      // console.log(req.headers)
+      
       const result = await usersCollection.find().toArray()
       res.send(result)
     })
+    //get single user and check is the "Admini" or no via variafytoke midlewire
+    app.get('/users/admin',varifyToken, async (req, res) => {
+      // const email = req.params.email
+      const email = req.query?.email
+      let admin = false
+      
+      const filter = { email: email }
+      // console.log(req.decoded,email)
+      
+      if (!email == req.decoded.email) {
+        return res.status(401).send({message:'forbidden access'})
+      }
+      const findUser =await usersCollection.findOne(filter)
+      console.log(findUser)
+      if (findUser) {
+        admin = findUser?.role ==='Admin'
+      }
+      res.send({admin})
+    })
+
     app.delete('/users/:id', async (req, res) => {
       const id = req.params.id
       const filter = { _id: new ObjectId(id) }
@@ -85,7 +134,6 @@ async function run() {
     app.patch('/users/:id', async (req, res) => {
       const id = req.params.id
       const body = req.body
-      console.log(id,body)
       const filter = {_id:new ObjectId(id)}
       const updateField = {
         $set: {
